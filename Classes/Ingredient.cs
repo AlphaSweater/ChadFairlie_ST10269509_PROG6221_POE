82aed18acf2a9e2ad02b9c61b21204931b6a419b
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-04-15 12:26:59 +0200

    Add project files.

---
+++ Classes/Ingredient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChadFairlie_ST10269509_PROG6221_POE.Classes
+{
+    internal class Ingredient
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChadFairlie_ST10269509_PROG6221_POE
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
