8c27bd334190775d9f5631577c0330142ad97631
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-04-15 13:19:49 +0200

    Added recipe creation functionality to BeginHere method
    Enhanced the `BeginHere` method to prompt the user for a recipe name and number of ingredients.
    A loop was added to collect ingredient details (name, quantity, unit) and create `Ingredient` objects, which are then added to the new `Recipe` object.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChadFairlie_ST10269509_PROG6221_POE.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,10 +19,31 @@ namespace ChadFairlie_ST10269509_PROG6221_POE
         {
             Console.WriteLine("---> Welcome to the Recipe Manager <---");
             Console.WriteLine("---------------------------------------");
+
             Console.WriteLine("Please enter the name of the recipe you would like to create:");
             Console.Write("/> ");
             string recipeName = Console.ReadLine();
             Classes.Recipe newRecipe = new Classes.Recipe(recipeName);
+
+            Console.WriteLine($"Great! Now let's add some ingredients to the {newRecipe.RecipeName} recipe.");
+            Console.WriteLine("Please enter the number of ingredients in the recipe:");
+            Console.Write("/> ");
+            int ingredientCount = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < ingredientCount; i++)
+            {
+                Console.WriteLine($"Please enter the name of ingredient number {i + 1}:");
+                Console.Write("/> ");
+                string ingredientName = Console.ReadLine();
+                Console.WriteLine("Please enter the quantity of this ingredient:");
+                Console.Write("/> ");
+                double ingredientQuantity = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the unit of measurement you would use for this ingredient:");
+                Console.WriteLine("Example: cups, grams, ounces, etc.");
+                Console.Write("/> ");
+                string ingredientUnit = Console.ReadLine();
+                Classes.Ingredient ingredient = new Classes.Ingredient(ingredientName, ingredientQuantity, ingredientUnit);
+                newRecipe.AddIngredient(ingredient);
+            }
         }
     }
 }
