a4913defc49796f91ffd48f4d8e64e5e0aaa9621
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-06-13 18:05:04 +0200

    Add HomeView and MainViewModel to MVVM structure
    This commit introduces a new HomeView UserControl and its corresponding MainViewModel to the project, following the MVVM pattern for enhanced separation of concerns. Specifically, the HomeView.xaml and HomeView.xaml.cs files have been added to the MVVM\View directory, establishing the initial UI for the application's home section.
    
    Similarly, the MainViewModel.cs file has been added to the MVVM\ViewModel directory, serving as a placeholder for future development of data binding and command implementation related to the HomeView.
    
    This setup indicates a strategic expansion of the application's UI and architecture.

---
+++ ChadFairlie_PROG6221_POE_GUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ChadFairlie_PROG6221_POE_GUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChadFairlie_PROG6221_POE_GUI.MVVM.ViewModel
+{
+	internal class MainViewModel : ObservableObject
+	{
+		public MainViewModel()
+		{
+		}
+	}
+}
