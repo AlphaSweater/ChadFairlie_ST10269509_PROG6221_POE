c03300ffa2452b0735d9063824582e76b4bb7a9d
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-06-16 22:11:43 +0200

    Removed Chats scrolling and replaced it with a basic but working scroll feature

--- ChadFairlie_PROG6221_POE_GUI/MVVM/View/HomeView.xaml.cs
+++ ChadFairlie_PROG6221_POE_GUI/MVVM/View/HomeView.xaml.cs
@@ -37,19 +37,6 @@ namespace ChadFairlie_PROG6221_POE_GUI.MVVM.View
 				var currentPosition = e.GetPosition(RecentRecipeScrollViewer);
 				var delta = currentPosition.X - _clickPosition.X;
 
-				// Reduce sensitivity of the drag scroll
-				delta *= 1.0;
-
-				// Translate the content smoothly
-				_translateTransform.X = delta;
-
-				// Determine if we need to apply elasticity
-				if (_initialHorizontalOffset + delta < 0 || _initialHorizontalOffset + delta > RecentRecipeScrollViewer.ScrollableWidth)
-				{
-					// Apply a reduced delta to simulate elasticity
-					delta *= 0.2;
-				}
-
 				// Smoothly scroll to the new offset
 				RecentRecipeScrollViewer.ScrollToHorizontalOffset(_initialHorizontalOffset - delta);
 			}
@@ -60,26 +47,9 @@ namespace ChadFairlie_PROG6221_POE_GUI.MVVM.View
 			if (sender is ScrollViewer)
 			{
 				Mouse.Capture(null);
-
-				// Animate the translation back to the original position if necessary
-				if (_translateTransform.X != 0)
-				{
-					var animation = new DoubleAnimation
-					{
-						To = 0,
-						Duration = TimeSpan.FromMilliseconds(400),
-						EasingFunction = new ElasticEase { EasingMode = EasingMode.EaseOut, Oscillations = 1, Springiness = 2 }
-					};
-					animation.Completed += (s, a) => ResetTransform(); // Reset transform and update offset
-					_translateTransform.BeginAnimation(TranslateTransform.XProperty, animation);
-				}
+				_translateTransform.X = 0;
+				_initialHorizontalOffset = RecentRecipeScrollViewer.HorizontalOffset;
 			}
-		}
-
-		private void ResetTransform()
-		{
-			_translateTransform.X = 0;
-			_initialHorizontalOffset = RecentRecipeScrollViewer.HorizontalOffset;
 		}
 	}
 }
